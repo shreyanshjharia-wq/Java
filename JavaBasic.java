c081e693616959f80674d616d945e65219e410d3
shreyanshjharia-wq <shreyanshjharia@gmail.com>
2026-09-07 14:48:20 +0530

    Initial Java programs

---
+++ JavaBasic.java
@@ -0,0 +1,12 @@
+public class JavaBasic {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 5;
+        if (a >= b) {
+            System.out.println("a is larger than 2");
+        } else {
+            System.out.println("b is larger than 2");
+        }
+    }
+    
+}
--- int.java
+++ int.java
@@ -1,8 +1,9 @@
-public class JavaBasics{
+class JavaBasics {
     public static void main(String args[]) {
-       int a = 10;
-         int b = 20;
-         System.out.println(a)
+        int a = 10;
+        int b = 20;
 
+        System.out.println(a);
+        System.out.println(b);
     }
 }
