54ede641285019aa761d639d99d3b896df980ed0
shreyanshjharia-wq <shreyanshjharia@gmail.com>
2026-09-09 14:10:16 +0530

    Add while loop program

---
+++ loops/whileloop.java
@@ -0,0 +1,10 @@
+public class whileloop {
+    public static void main(String[] args) {
+        // to print hello world 100 times using while loop
+        int counter = 0;
+        while (counter < 100) {
+            System.out.println("Hello World");
+            counter++;
+        }
+}
+}
