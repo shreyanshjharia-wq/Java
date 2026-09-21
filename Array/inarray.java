9b834b7fb990a831773bffcc19c0e9a43061bbbe
shreyanshjharia-wq <shreyanshjharia@gmail.com>
2026-09-21 21:03:49 +0530

    Add array program

---
+++ Array/inarray.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class inarray {
+    public static void main(String args[]) {
+
+        int marks[] = new int[100];
+
+        Scanner sc = new Scanner(System.in);
+
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println("Physics Marks : " + marks[0]);
+        System.out.println("Chemistry Marks : " + marks[1]);
+        System.out.println("Maths Marks : " + marks[2]);
+    }
+}
