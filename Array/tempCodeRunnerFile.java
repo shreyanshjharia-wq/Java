5c554f2ecd1f4284e4f96711de106bd950e1948f
shreyanshjharia-wq <shreyanshjharia@gmail.com>
2026-09-22 01:00:21 +0530

    Add array program

---
+++ Array/tempCodeRunnerFile.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class updateinarray {
+    public static void main(String args[]) {
+
+        int marks[] = new int[100];
+
+        Scanner sc = new Scanner(System.in);
+
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println("Physics Marks : " + marks[0]);
+        System.out.println("Chemistry Marks : " + marks[1]);
+        System.out.println("Maths Marks : " + marks[2]);
+
+        marks[0] = 90; // Updating Physics Marks
+        marks[1] = marks[2] + 1; // Updating Chemistry Marks
+        marks[2] = 100; // Updating Maths Marks
+
+        System.out.println("Updated Physics Marks : " + marks[0]);
+        System.out.println("Updated Chemistry Marks : " + marks[1]);
+        System.out.println("Updated Maths Marks : " + marks[2]);
+    }
+}
