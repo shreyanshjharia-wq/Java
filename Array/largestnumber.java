6f1aa276047d8866cd73c06df291189e10ad078d
shreyanshjharia-wq <shreyanshjharia@gmail.com>
2026-09-24 23:36:03 +0530

    Add array program

---
+++ Array/largestnumber.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class largestnumber {
+    public static int get largest(int number[]){
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+        for 
+    }
+    
+}
